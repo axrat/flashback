e0282b536f17cbbb1b2fee73395c99467d715db9
onoie <onoie3@gmail.com>
2017-09-04 16:31:08 +0900

    commit from perl

--- update.pl
+++ update.pl
@@ -1,5 +1,12 @@
 #!/usr/bin/perl
+
+($mday,$mon,$year) = (localtime(time))[3..5];
+$year += 1900;
+$mon += 1;
+print "$year/$mon/$mday\n";
+
 #open(DATAFILE, "> touch") or die("error :$!");
+
 system("git add .");
 system('git commit -m "commit from perl"');
 system('git push');
